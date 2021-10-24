25f2ce1db318a276d8f907a64285b4f944faea05
Viktor Sopko <najetak.viktrixeu@gmail.com>
2021-10-24 10:02:13 +0200

    Adding swiper,fixing counter bug.

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
 function showOnClick() {
     var x = document.getElementById("cart");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
+    if (x.style.display === "block") {
         x.style.display = "none";
+    } else {
+        x.style.display = "block";
     }
 }
 
+//initialising a variable name data
+
+var data = 0;
+
+//printing default value of data that is 0 in h2 tag
+document.getElementById("counter-label").innerText = data;
+
+//creation of increment function
 function increment() {
-    var x = document.getElementById('plus-icon').innerHTML;
-    x++;
-    document.getElementById('counter-placeholder').innerHTML = x;
+    data += 1;
+    document.getElementById("counter-label").innerText = data;
+}
+//creation of decrement function
+function decrement() {
+    if (data > 0) data -= 1;
+    document.getElementById("counter-label").innerText = data;
 }
 
-function decrement() {
-    var x = document.getElementById('minus-icon').innerHTML;
-    x++;
-    document.getElementById('counter-placeholder').innerHTML = x;
-}
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
