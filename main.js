e9de09447b42007a960171c6918250d6854819d2
Viktor Sopko <najetak.viktrixeu@gmail.com>
2021-10-25 19:50:52 +0200

    Fixing bug.

--- main.js
+++ main.js
@@ -67,3 +67,96 @@ function showCart() {
     else
         x.style.display = 'none'
 }
+
+function borderImage1() {
+    let x = document.getElementById('image4');
+    let w = document.getElementById('image3');
+    let z = document.getElementById('image2');
+    let y = document.getElementById('image1');
+
+    let xBIg = document.getElementById('image4-big');
+    let wBIg = document.getElementById('image3-big');
+    let zBIg = document.getElementById('image2-big');
+    let yBIg = document.getElementById('image1-big');
+
+    y.style.border = '2px solid var(--primary-color)';
+    y.style.filter = 'grayscale(50%)';
+    y.style.filter = 'blur(0.5px)';
+    yBIg.style.display = 'inline-block';
+
+    x.style.border = 'none';
+    z.style.border = 'none';
+    w.style.border = 'none';
+    xBIg.style.display = 'none';
+    zBIg.style.display = 'none';
+    wBIg.style.display = 'none';
+}
+function borderImage2() {
+    let x = document.getElementById('image4');
+    let w = document.getElementById('image3');
+    let z = document.getElementById('image2');
+    let y = document.getElementById('image1');
+
+    let xBIg = document.getElementById('image4-big');
+    let wBIg = document.getElementById('image3-big');
+    let zBIg = document.getElementById('image2-big');
+    let yBIg = document.getElementById('image1-big');
+
+    z.style.border = '2px solid var(--primary-color)';
+    z.style.filter = 'grayscale(50%)';
+    z.style.filter = 'blur(0.5px)';
+    zBIg.style.display = 'inline-block';
+
+    y.style.border = 'none';
+    w.style.border = 'none';
+    x.style.border = 'none';
+    yBIg.style.display = 'none';
+    wBIg.style.display = 'none';
+    xBIg.style.display = 'none';
+}
+function borderImage3() {
+    let x = document.getElementById('image4');
+    let w = document.getElementById('image3');
+    let z = document.getElementById('image2');
+    let y = document.getElementById('image1');
+
+    let xBIg = document.getElementById('image4-big');
+    let wBIg = document.getElementById('image3-big');
+    let zBIg = document.getElementById('image2-big');
+    let yBIg = document.getElementById('image1-big');
+
+    w.style.border = '2px solid var(--primary-color)';
+    w.style.filter = 'grayscale(50%)';
+    w.style.filter = 'blur(0.5px)';
+    wBIg.style.display = 'inline-block';
+
+    y.style.border = 'none';
+    z.style.border = 'none';
+    x.style.border = 'none';
+    yBIg.style.display = 'none';
+    zBIg.style.display = 'none';
+    xBIg.style.display = 'none';
+}
+function borderImage4() {
+    let x = document.getElementById('image4');
+    let w = document.getElementById('image3');
+    let z = document.getElementById('image2');
+    let y = document.getElementById('image1');
+
+    let xBIg = document.getElementById('image4-big');
+    let wBIg = document.getElementById('image3-big');
+    let zBIg = document.getElementById('image2-big');
+    let yBIg = document.getElementById('image1-big');
+
+    x.style.border = '2px solid var(--primary-color)';
+    x.style.filter = 'grayscale(50%)';
+    x.style.filter = 'blur(0.5px)';
+    xBIg.style.display = 'inline-block';
+
+    y.style.border = 'none';
+    z.style.border = 'none';
+    w.style.border = 'none';
+    yBIg.style.display = 'none';
+    zBIg.style.display = 'none';
+    wBIg.style.display = 'none';
+}
